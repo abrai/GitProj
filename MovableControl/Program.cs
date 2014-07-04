59f86298c17503e5656cf98eb7105f3f13cb31b6
abrai <rai.abhishek@yahoo.com>
2014-07-05 00:17:22 +0530

    test check in

--- MovableControl/Program.cs
+++ MovableControl/Program.cs
@@ -8,7 +8,7 @@ namespace MovableControl
     {
         /// <summary>
         /// The main entry point for the application.
-        /// test git extension
+        /// test git extension for edit file
         /// </summary>
         [STAThread]
         static void Main()
